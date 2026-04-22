a7320401ad5a22a379459f812bf063b0ddfa566a
Andres Mosquera Alvarado <qualitycolombia.dvp09@gmail.com>
2026-04-22 17:57:07 -0500

    Adding java endpoints

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor(private readonly service: OrdersService) {}
+
+    @Post("create")
+  create(@Body() body: any) {
+    return this.service.createOrder(body);
+  }
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(private readonly http: HttpService) {}
+
+
+    async createOrder(data: any) {
+    const url = 'http://18.118.49.251:8080/orders';
+
+    const response = await firstValueFrom(
+      this.http.post(url, data),
+    );
+
+    return response.data;
+  }
+}
